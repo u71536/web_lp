fc11f8a9c3c4c616577e34739e0dbeb660666511
d.aristakesian <d.aristakesian@corp.nekki.ru>
2024-03-11 17:26:21 +0300

    new version

--- learn_web/delete_user.py
+++
@@ -1,6 +0,0 @@
-from db import db_session
-from models import User
-
-my_user = User.query.first()
-db_session.delete(my_user)
-db_session.commit()
--- learn_web/update_user.py
+++
@@ -1,6 +0,0 @@
-from db import db_session
-from models import User
-
-my_user = User.query.first()
-my_user.salary = 200
-db_session.commit()
