0405044bf33742783d5a03f06e6249a548e590e8
Djivan <u71536@gmail.com>
2024-03-12 20:29:30 +0300

    добавил базу данных

---
+++ create_db.py
@@ -0,0 +1,4 @@
+from webapp import db, create_app
+
+with create_app().app_context():
+    db.create_all()
--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
 
 from webapp.news import get_python_news
-from webapp.model import db
+from webapp.model import db, News
 from webapp.weather import weather_by_city
+
 
 def create_app():
     app = Flask(__name__)
@@ -13,7 +14,7 @@ def create_app():
     def index():
         page_title = "Супер сайт"
         weather = weather_by_city(app.config["WEATHER_DEFAULT_CITY"])
-        news_list = get_python_news()
+        news_list = News.query.order_by(News.published.desc()).all()
         return render_template('index.html', page_title=page_title, weather=weather, news_list=news_list)
 
     return app
